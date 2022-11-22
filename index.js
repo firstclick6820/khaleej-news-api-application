b9376051bd1fbeb983206599a29f55adb32c37ac
Khalid Momand <mohammadkhalidmomand@gmail.com>
2022-11-22 18:51:57 +0400

    Index.js File is configured

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-// create an express app
+// Define the PORT
+const PORT= 5000
+
+// Import required Modules
 const express = require("express")
+const cheerio = require('cheerio')
+const axios = require('axios')
+
+
+
+// Initilize the app
 const app = express()
 
 // use the express-static middleware
 app.use(express.static("public"))
+
+
+
+
 
 // define the first route
 app.get("/", function (req, res) {
   res.json("Now it is working")
 })
 
+
+
+// define the second route
+app.get("/news", function (req, res) {
+    res.json('News Section')
+})
+
+
+
+
+
+
+
+
+
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || PORT, 
+	() => console.log(`Server is running on PORT ${PORT}...`));
