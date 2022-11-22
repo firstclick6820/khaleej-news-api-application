d501cc8da400f26a16063ee54804825ca6ee606f
Khalid Momand <mohammadkhalidmomand@gmail.com>
2022-11-22 18:37:12 +0400

    The Basic server is created

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+// import modules
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
+// Define the port
+const PORT = 5000
+
+// Initilize the app
+const app = express()
+
+// Define the initial URL
+
+app.get('/', (req, res)=> {
+    res.json('Hello, World')
+})
+
+
+
+
+app.listen(PORT, ()=> {console.log(`Server is running on PORT ${PORT}`)})
