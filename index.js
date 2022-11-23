49a5f52e18123e098ec9c542e20e7cfff515a066
Khalid Momand <mohammadkhalidmomand@gmail.com>
2022-11-23 15:40:03 +0400

    Header is configured

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ const app = express()
 
 
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Define our News API
 const khaleejNewsApi = []
